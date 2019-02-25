a813c1c208b68d584800e200ffd715c2de9048d1
LucasManciniDaSilva <lucasmancini1997@gmail.com>
2019-02-25 12:05:10 -0300

    CREATE A NEW PROGRAM FOR SHOW THE FOLDERS, FILES AND CREATE A NEW FOLDER

---
+++ Arquivos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+namespace Arquvios
+{
+  class Program
+        {
+            static void Main(string[] args)
+            {
+            string path = @"/users/lucasmancini/downloads/Strider";
+            try
+            {
+                var Folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach(string f in Folders)
+                {
+                    Console.WriteLine(f);
+                }
+
+                var Files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES: ");
+                foreach(string s in Files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Directory.CreateDirectory(@"/users/lucasmancini/downloads/Strider/Dragon Ball Ultimate Battle 22");
+            }
+
+            catch(IOException e)
+            {
+                Console.WriteLine("DEU ERRO AI CHARA");
+                Console.WriteLine(e);
+            }
+        }
+        }
+        }
+
